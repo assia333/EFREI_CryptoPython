64656969bdff36dded3ea34a96977d21374b3925
assia333 <88588191+assia333@users.noreply.github.com>
2025-10-27 15:42:27 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')   #helloWold))
+    return render_template('hello.html')   #helloWold)))))
 
 key = Fernet.generate_key()
 f = Fernet(key)
